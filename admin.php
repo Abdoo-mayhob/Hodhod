edfab2cdd4313744a4b70c85b2e401fee8eef476
a.almayhob <a.almayhob@haykalmedia.com>
2024-06-22 18:22:31 +0300

    Working MVP

--- admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-// If this file is called directly, abort.
-defined( 'ABSPATH' ) or die;
-
-
-
-// The Default Value for each of the options
-$settings_defaults = self::SETTINGS_DEFAULT;
--- hodhod.php
+++ hodhod.php
@@ -34,6 +34,17 @@
 // If this file is called directly, abort.
 defined( 'ABSPATH' ) or die;
 
+// Settings link in beside the plugin name in plugins admin screen
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), function ($links) {
+	return array_merge(
+		[ '<a href="' . esc_url(
+			add_query_arg(
+				[ 'autofocus[section]' => 'hodhod_section' ], admin_url( 'customize.php' )
+			)
+		) . '">Settings</a>' ],
+		$links );
+} );
+
 // Load Translation Files (Translations only needed in admin area)
 add_action( 'plugins_loaded', function () {
 	load_plugin_textdomain( 'hodhod', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
@@ -50,11 +61,14 @@ add_action( 'init', function () {
  */
 class Hodhod {
 
-	public const SETTINGS = 'hodhod';
+	public const OPTION = 'hodhod';
 
 	// Plugin Settings and Default Values (Used when options not set yet)
 	public $settings = [];
-	public const SETTINGS_DEFAULT = [
+	public const SETTINGS_DEFAULT = [ 
+		'iframe' => '',
+		'show_profile' => false,
+		'dark_mode' => true,
 	];
 
 	// Refers to a single instance of this class
@@ -71,31 +85,141 @@ class Hodhod {
 	}
 
 	public function __construct() {
-		add_action( 'admin_menu', [ $this, 'admin_menu' ] );
+		add_action( 'customize_register', [ $this, 'customizer' ] );
 		add_shortcode( 'hodhod', [ $this, 'shortcode' ] );
 
 		// Load Plugin Settings 
-		$this->settings = get_option( self::SETTINGS, self::SETTINGS_DEFAULT );
+		$this->settings = get_option( self::OPTION, self::SETTINGS_DEFAULT );
 	}
 
 	// --------------------------------------------------------------------------------------
 	// Admin Menu
-	public function admin_menu() {
-		add_options_page(
-			__( 'Hodhod Settings', 'hodhod' ),
-			__( 'Hodhod', 'hodhod' ),
-			'manage_options',
-			'hodhod',
-			[ $this, 'view_admin' ]
-		);
+
+	public function customizer( $wp_customize ) {
+
+
+		$wp_customize->add_section( 'hodhod_section', [ 
+			'title' => __( 'Hodhod Settings', 'hodhod' ),
+			'priority' => 30,
+		] );
+
+		// iframe
+		$wp_customize->add_setting( self::OPTION . '[iframe]', [ 
+			'type' => 'option',
+			'default' => self::SETTINGS_DEFAULT['iframe'],
+			'sanitize_callback' => [ $this, 'kses' ], // This allows HTML tags
+		] );
+		$wp_customize->add_control( new WP_Customize_Code_Editor_Control(
+			$wp_customize,
+			self::OPTION . '[iframe]',
+			[ 
+				'label' => __( 'Iframe', 'hodhod' ),
+				'section' => 'hodhod_section',
+				'code_type' => 'text/html',
+			]
+		) );
+
+		// show_profile checkbox
+		$wp_customize->add_setting( self::OPTION . '[show_profile]', [ 
+			'type' => 'option',
+			'default' => self::SETTINGS_DEFAULT['show_profile'],
+			'sanitize_callback' => 'rest_sanitize_boolean',
+		] );
+		$wp_customize->add_control( self::OPTION . '[show_profile]', [ 
+			'label' => __( 'Show Profile', 'hodhod' ),
+			'section' => 'hodhod_section',
+			'type' => 'checkbox',
+		] );
+
+		// dark_mode checkbox
+		$wp_customize->add_setting( self::OPTION . '[dark_mode]', [ 
+			'type' => 'option',
+			'default' => self::SETTINGS_DEFAULT['dark_mode'],
+			'sanitize_callback' => 'rest_sanitize_boolean',
+		] );
+		$wp_customize->add_control( self::OPTION . '[dark_mode]', [ 
+			'label' => __( 'Dark Mode', 'hodhod' ),
+			'section' => 'hodhod_section',
+			'type' => 'checkbox',
+		] );
 	}
 
-	public function view_admin( $post ) {
-		require_once __DIR__ . '/admin.php';
+	// --------------------------------------------------------------------------------------
+	// Getters
+
+	/**
+	 * Our own version of wp_kses where we allow iframes
+	 *
+	 * @param string
+	 * @return string
+	 */
+	public static function kses( $value ) {
+		$allowed_html = wp_kses_allowed_html( 'post' );
+		$allowed_html['iframe'] = [ 
+			'src' => true,
+			'width' => true,
+			'height' => true,
+			'align' => true,
+			'class' => true,
+			'name' => true,
+			'id' => true,
+			'frameborder' => true,
+			'seamless' => true,
+			'srcdoc' => true,
+			'sandbox' => true,
+			'allowfullscreen' => true
+		];
+		return wp_kses( $value, $allowed_html );
 	}
 
+	public function get_iframe() {
+
+		$doc = new DOMDocument();
+		$doc->loadHTML( $this->settings['iframe'] );
+		$iframe = $doc->getElementsByTagName( 'iframe' );
+
+		// Validation
+		if ( $iframe->length === 0 ) {
+			return false; // No iframe tag found
+		}
+
+		$iframe = $iframe->item( 0 );
+		$src = $iframe->getAttribute( 'src' );
+		ldd( $src, 'src' );
+		// Parse the URL and query string
+		$url_parts = parse_url( $src );
+
+		// Validation
+		if ( ! ( isset( $url_parts['host'] ) && $url_parts['host'] === 'gohodhod.com' ) ) {
+			return false;
+		}
+		ldd( $url_parts, 'url_parts' );
+
+		parse_str( $url_parts['query'], $params );
+
+		// Now you can modify the query params as needed
+		$show_profile = $this->settings['show_profile'] ? 'false' : 'true';
+		$dark_mode = $this->settings['dark_mode'] ? 'dark' : 'light';
+		$params['withoutProfile'] = $show_profile;
+		$params['theme'] = $dark_mode;
+
+		// Build the new query string and replace it in the original URL
+		$url_parts['query'] = http_build_query( $params );
+		$new_src = $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'] . '?' . $url_parts['query'];
+
+		// Set the new src attribute
+		$iframe->setAttribute( 'src', $new_src );
+
+		// Save the modified HTML
+		$new_iframe = $doc->saveHTML( $iframe );
+
+		return $new_iframe;
+	}
+
+	// --------------------------------------------------------------------------------------
+	// ShortCodes
 	public function shortcode() {
-		return wp_kses_post( '' );
+		return self::kses($this->get_iframe());
 	}
 
 	// --------------------------------------------------------------------------------------
